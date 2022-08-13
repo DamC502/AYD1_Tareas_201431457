cad6a2d10fe1998e74f2ecebc162ac5590ec67a1
damihan <dmdm0123@gmail.com>
2022-08-12 20:41:38 -0600

    Se ha añadido el endpoint para informacion

--- Tarea1/cmd/api/main.go
+++ Tarea1/cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -28,11 +29,18 @@ type application struct {
 
 func main() {
 	var cfg config
+	var cfg2 config
 	cfg = config{
 		port: 3000,
 		env:  "development",
 	}
-	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+
+	cfg2 = config{
+		port: 4000,
+		env:  "development",
+	}
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	app := &application{
 		config: cfg,
@@ -40,19 +48,43 @@ func main() {
 	}
 
 	server := &http.Server{
-		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: app.routes(),
-		IdleTimeout: time.Minute,
-		ReadTimeout:  10* time.Second,
-		WriteTimeout: 10* time.Second,
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
-
-	logger.Println("Starting server on port", cfg.port)
-	err := server.ListenAndServe()
-
-	if err != nil {
-		log.Println(err)
+	server2 := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg2.port),
+		Handler:      app.routesInfo(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
-	return
+
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+
+	go func() {
+		logger.Println("Starting server on port", cfg.port)
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Println(err)
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		logger.Println("Starting server on port", cfg2.port)
+		err := server2.ListenAndServe()
+		if err != nil {
+			log.Println(err)
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+
 }
--- Tarea1/cmd/api/routes.go
+++ Tarea1/cmd/api/routes.go
@@ -5,8 +5,14 @@ import (
 	"net/http"
 )
 
-func (app *application)routes () http.Handler{
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
-	router.HandlerFunc(http.MethodPost,"/suma",app.CalculatorPost)
+	router.HandlerFunc(http.MethodPost, "/suma", app.CalculatorPost)
+	return app.enableCORS(router)
+}
+
+func (app *application) routesInfo() http.Handler {
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/info", app.InfoGet)
 	return app.enableCORS(router)
 }
